6d7255299b7ed8dbe804b01bd3a7ce5d48e4b033
krishna2803 <kpandey81930@gmail.com>
2025-03-01 19:45:07 +0530

    chore: update constructor
    Signed-off-by: krishna2803 <kpandey81930@gmail.com>

--- src/bfloat16.h
+++ src/bfloat16.h
@@ -18,14 +18,28 @@ using u64 = uint16_t;
 
 class bfloat16 {
 private:
-  u16 value;
+  explicit constexpr bfloat16(const u16 u = 0) { this->_value = u; }
 
-  explicit bfloat16(u16 u = 0);
+  explicit constexpr bfloat16(const u32 u = 0)
+      : bfloat16(static_cast<u16>(u)) {}
 
 public:
+  u16 _value;
+
   bfloat16(const bfloat16 &) = default;
+  bfloat16 &operator=(const bfloat16 &) = default;
+  bfloat16(float f = 0.0f);
+
+  static constexpr bfloat16 INF() { return bfloat16(0b0'11111111'0000000u); }
+
+  static constexpr bfloat16 NEG_INF() {
+    return bfloat16(0b1'11111111'0000000u);
+  }
+
+  static constexpr bfloat16 NAN() { return bfloat16(0b1'11111111'0000001u); }
 
   explicit bfloat16(float f = 0.0f);
+  static constexpr bfloat16 ZERO() { return bfloat16(0u); }
 
   [[nodiscard]]
   auto get_val() const -> u16;
